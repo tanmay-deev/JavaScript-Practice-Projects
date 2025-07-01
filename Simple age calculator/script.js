55a81f8a4054839b528664f2a6c00e25dd60bcf0
tanmay-deev <tanmaybonde10@gmail.com>
2025-07-01 13:12:55 +0530

    Age claculator

--- Simple age calculator/script.js
+++ Simple age calculator/script.js
@@ -1,5 +1,6 @@
 let userInput = document.getElementById("date");
 userInput.max = new Date().toISOString().split("T")[0];
+let result = document.getElementById("result");
 
 function calculateAge(){
     let birthDate = new Date(userInput.value);
@@ -31,6 +32,11 @@ function calculateAge(){
         m3--;
         d3 = getDaysInMonth(y1, m1) = d2 - d1;
     }
+    if(m3 < 0){
+        m3 = 11;
+        y3--;
+    }
+    result.innerHTML = `You are <span>${y3}</span> years, <span>${m3}</span> months and <span>${d3}</span> days old`;
 }
 
 function getDaysInMonth(year, month){
